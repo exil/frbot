d461c9ee667b63c32465d0354eb958a93ae4166f
Max Pappas <mpappas>
2016-10-31 01:45:38 -0700

    creating new discord bot

---
+++ app.js
@@ -0,0 +1,15 @@
+var Discord = require("discord.js");
+var config = require('./config');
+var bot = new Discord.Client();
+
+bot.on("message", msg => {
+    if (msg.content.startsWith("ping")) {
+        msg.channel.sendMessage("pong!");
+    }
+});
+
+bot.on('ready', () => {
+  console.log('I am ready!');
+});
+
+bot.login(config.BOT_TOKEN);
